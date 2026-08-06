67db4a3ffc8e04034cbe3f8a43aec927eb97c823
tenzinplatter <143778894+TenzinPlatter@users.noreply.github.com>
2026-08-06 14:44:40 +1000

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/review.lua

--- .config/nvim/lua/tenzin/plugins/review.lua
+++ .config/nvim/lua/tenzin/plugins/review.lua
@@ -1,4 +1,28 @@
+-- local review = require("review")
+-- local qc = require("review.quick_comments")
+
 return {
-    "vuki656/review.nvim",
-    opts = {},
+  "vuki656/review.nvim",
+  cmd = "Review",
+  keys = {
+    { "<leader>rv", "<cmd>Review<cr>", desc = "Toggle review" },
+    -- {
+    --   "<leader>rc",
+    --   qc.add,
+    --   desc = "Add comment on current line",
+    -- },
+    -- {
+    --   "<leader>rc",
+    --   qc.add_visual,
+    --   desc = "Add comment on visual selection",
+    --   mode = "v",
+    -- },
+    -- {
+    --   "<leader>re",
+    --   qc.export,
+    --   desc = "Export review to clipboard",
+    --   mode = "v",
+    -- },
+  },
+  opts = {},
 }
