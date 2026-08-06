d67a0df2b3d267e7f0170e68157d12a46b9f649f
tenzinplatter <143778894+TenzinPlatter@users.noreply.github.com>
2026-08-06 16:58:54 +1000

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/review.lua

--- .config/nvim/lua/tenzin/plugins/review.lua
+++ .config/nvim/lua/tenzin/plugins/review.lua
@@ -1,28 +1,11 @@
--- local review = require("review")
--- local qc = require("review.quick_comments")
-
 return {
   "vuki656/review.nvim",
-  cmd = "Review",
-  keys = {
-    { "<leader>rv", "<cmd>Review<cr>", desc = "Toggle review" },
-    -- {
-    --   "<leader>rc",
-    --   qc.add,
-    --   desc = "Add comment on current line",
-    -- },
-    -- {
-    --   "<leader>rc",
-    --   qc.add_visual,
-    --   desc = "Add comment on visual selection",
-    --   mode = "v",
-    -- },
-    -- {
-    --   "<leader>re",
-    --   qc.export,
-    --   desc = "Export review to clipboard",
-    --   mode = "v",
-    -- },
-  },
-  opts = {},
+  config = function()
+    require("review").setup()
+    local qc = require("review.quick_comments")
+    vim.keymap.set("n", "<leader>rv", "<cmd>Review<cr>", { desc = "Toggle review" })
+    vim.keymap.set("n", "<leader>rc", qc.add, { desc = "Add comment on current line" })
+    vim.keymap.set("v", "<leader>rc", qc.add_visual, { desc = "Add comment on visual selection" })
+    vim.keymap.set("n", "<leader>re", qc.export, { desc = "Export review to clipboard" })
+  end,
 }
