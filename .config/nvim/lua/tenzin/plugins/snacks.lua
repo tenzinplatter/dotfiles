6be8f6f5108bfac932f278edeca85d1422b55261
tenzinplatter <143778894+TenzinPlatter@users.noreply.github.com>
2026-08-11 16:33:23 +1000

    Modified 2
    Modified:
      config/herdr/herdr-server.log
      .config/nvim/lua/tenzin/plugins/snacks.lua

--- .config/nvim/lua/tenzin/plugins/snacks.lua
+++ .config/nvim/lua/tenzin/plugins/snacks.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    bigfile = { enabled = false },
+    bigfile = { enabled = true },
     dashboard = {
       enabled = true,
       preset = {
