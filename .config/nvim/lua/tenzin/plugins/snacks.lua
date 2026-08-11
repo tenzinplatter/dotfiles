286818030d3d18a08f4dcf9a7881e0e4b41f31b5
tenzinplatter <143778894+TenzinPlatter@users.noreply.github.com>
2026-08-11 16:32:51 +1000

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/snacks.lua

--- .config/nvim/lua/tenzin/plugins/snacks.lua
+++ .config/nvim/lua/tenzin/plugins/snacks.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    bigfile = { enabled = true },
+    bigfile = { enabled = false },
     dashboard = {
       enabled = true,
       preset = {
