7530c4a3cc5b6ede231146d28233a668d49ffa3f
tenzinplatter <143778894+TenzinPlatter@users.noreply.github.com>
2026-09-25 17:32:47 +1000

    Modified 4
    Modified:
      config/kitty/dank-tabs.conf
      .config/niri/dms/colors.kdl
      .config/nvim/colors/dms.lua
      .config/zed/themes/dank-zed-theme.json

--- .config/nvim/colors/dms.lua
+++ .config/nvim/colors/dms.lua
@@ -86,9 +86,9 @@ end
 
 if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~= vim.o.background then
 	local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
-	local harmonized = base46.theme_harmonize(builtin, "#d0bcff", harmony)
+	local harmonized = base46.theme_harmonize(builtin, "#ad675c", harmony)
 	if settings.matugenTemplateNeovimSetBackground ~= false then
-		harmonized = base46.theme_set_bg(harmonized, "#141218")
+		harmonized = base46.theme_set_bg(harmonized, "#1a1110")
 	end
 
 	base46.theme_tables[theme_name] = harmonized
